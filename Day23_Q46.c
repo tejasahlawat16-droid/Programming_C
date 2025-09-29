0110ff4de7e338cdc28a4fe28bea721bade15932
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2025-09-29 21:12:19 +0530

    Write a program to print the following pattern: ***** ***** ***** ***** *****

---
+++ Day23_Q46.c
@@ -0,0 +1,24 @@
+/* Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Outer loop for rows
+    for(i = 1; i <= 5; i++) {
+        // Inner loop for columns
+        for(j = 1; j <= 5; j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to next line after each row
+    }
+
+    return 0;
+}
