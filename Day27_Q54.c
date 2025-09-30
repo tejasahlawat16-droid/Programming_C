0bb1161e13c01d0ed42ad015c18a7fb626aad07b
tejasahlawat16-droid <tejasahlawat16@gmail.com>
2025-09-30 23:38:30 +0530

    Write a program to print the following pattern:
       *
      ***
     *****
    *******
     *****
      ***
       *

---
+++ Day27_Q54.c
@@ -0,0 +1,46 @@
+/* Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 4; // number of rows in the upper half (excluding lower half)
+
+    // Upper half (1,3,5,7 stars)
+    for (i = 1; i <= n; i++) {
+        // print spaces
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        // print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half (5,3,1 stars)
+    for (i = n - 1; i >= 1; i--) {
+        // print spaces
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        // print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
